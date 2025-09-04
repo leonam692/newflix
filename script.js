e7a7b08f37197a73416057e915074df7425037dc
leonam692 <leonam.amaral@escola.pr.gov.br>
2025-09-04 09:06:28 -0300

    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorALL('.btn-proximo');
+const reiniciarBtn = document.getElementByIdmentByid
+
+avanca.forEach(button => {
+ button.addEventListener('clic',function(){
+    const atual = document.querySelector('ativo');
+    const proximoPasso='passo-'+this.getAttribute('Data-proximo');
+    
+ })   
+
+});
